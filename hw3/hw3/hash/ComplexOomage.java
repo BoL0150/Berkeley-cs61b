70d5ea83b6dc33e3a4f2182a86c4820372de6eaa
BoLee15 <1160360517@qq.com>
2021-04-16 13:56:29 +0800

    hw3

--- hw3/hw3/hash/ComplexOomage.java
+++ hw3/hw3/hash/ComplexOomage.java
@@ -13,7 +13,7 @@ public class ComplexOomage implements Oomage {
     public int hashCode() {
         int total = 0;
         for (int x : params) {
-            total = total * 256;
+            total = total * 31;
             total = total + x;
         }
         return total;
